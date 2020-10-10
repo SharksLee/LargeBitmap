6ae2920ce8b75866138e2d9f77a5a9ce8ca23b37
wb-lsj02132861 <wb-lsj02132861@alibaba-inc.com>
2020-10-10 14:26:06 +0800

    asm imageview setBackgroudDrawable

---
+++ app/src/main/java/com/gavin/asmdemo/ImageViewDemo.java
@@ -0,0 +1,11 @@
+package com.gavin.asmdemo;
+
+import android.support.v7.widget.AppCompatImageView;
+
+/**
+ * @description
+ * @author: lishaojie
+ * @date: 2020/10/9
+ */
+public class ImageViewDemo extends AppCompatImageView {
+}
